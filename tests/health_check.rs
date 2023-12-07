495336c226d3989d5cc3a24a8476d2401cb5572b
Nelson Lee <leelhn2345@outlook.com>
2023-12-07 14:49:08 +0800

    feat: add db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,17 @@
 use tokio::net::TcpListener;
 
+/// spawns a separate app on a random port for testing
+async fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .await
+        .expect("failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+
+    tokio::spawn(axum_prod::run(listener));
+
+    format!("http://127.0.0.1:{port}")
+}
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app().await;
@@ -16,13 +28,52 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app().await;
+
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
         .await
-        .expect("failed to bind to random port");
-    let port = listener.local_addr().unwrap().port();
+        .expect("Failed to execute request.");
 
-    tokio::spawn(axum_prod::run(listener));
+    // assert
+    assert_eq!(200, response.status().as_u16())
+}
 
-    format!("http://127.0.0.1:{port}")
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    // Arrange
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        // Assert
+        assert_eq!(
+            422,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 422 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
