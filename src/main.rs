8743401f774c438a3d24ccf8cd406c268bfc119b
Nelson Lee <leelhn2345@outlook.com>
2024-01-28 18:20:58 +0800

    chore: reset everything

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 #![warn(clippy::pedantic)]
 
-use axum_prod::run;
-use tokio::net::TcpListener;
-
-#[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3000")
-        .await
-        .expect("failed to bind to random port");
-
-    run(listener).await;
+fn main() {
+    println!("hello world!");
 }
